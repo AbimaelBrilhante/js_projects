274c4c57fac42399c57cb689833995d81ccf3a26
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2022-08-03 16:48:55 -0300

    update html e js

--- gerador_de_senhas/script.js
+++ gerador_de_senhas/script.js
@@ -1,13 +1,37 @@
 function getPassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
-    var passwordLength = 16;
+    var chars = "";
+    var numeros = "0123456789"
+    var alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ"
+    var caracteres_especiais = "!@#$%^&*()+?><:{}[]"
+    var passwordLength = document.getElementById("length").value;
+    var checkbox1 = document.getElementById("n")
+    var checkbox2 = document.getElementById("a")
+    var checkbox3 = document.getElementById("geral")
     var password = "";
+
+    if(checkbox3.checked) {
+      chars = numeros+alfabeto+caracteres_especiais
+  } 
+    else if(checkbox2.checked){
+      chars = alfabeto + numeros
+    }
+  
+    else if (checkbox1.checked) {
+      chars = numeros
+  }
+
+    else{
+      window.alert("Escolha uma Opção");
+    }
+
 
     for (var i = 0; i < passwordLength; i++) {
       var randomNumber = Math.floor(Math.random() * chars.length);
       password += chars.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById('password').value = password
+    
   }
-
+ 
   
+
